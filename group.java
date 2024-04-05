d7af927bb62b87edc8797430381e3dd92925a0ed
brandoncw22 <162731539+brandoncw22@users.noreply.github.com>
2024-04-04 22:12:24 -0400

    Update group.java

--- group.java
+++ group.java
@@ -2,7 +2,7 @@ public class group {
 	public void getGroupMembers() {
 		System.out.println("Anthony Mikulcic");
 		System.out.println("Brandon Wendell");
-        System.out.println("Drew Robinson");
+        	System.out.println("Drew Robinson");
 	}
 	
 
