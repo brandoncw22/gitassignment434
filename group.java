c4f113f8a9e5e8273018fed6a5afa5c841074e08
Drob11 <115478064+Drob11@users.noreply.github.com>
2024-04-04 22:12:02 -0400

    Update group.java

--- group.java
+++ group.java
@@ -2,6 +2,7 @@ public class group {
 	public void getGroupMembers() {
 		System.out.println("Anthony Mikulcic");
 		System.out.println("Brandon Wendell");
+        System.out.println("Drew Robinson");
 	}
 	
 
