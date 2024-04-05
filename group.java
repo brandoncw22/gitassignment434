6fb526c83abb868a40459a59ed055772ee109bda
Dylan Monreal <dylanmonreal1@gmail.com>
2024-04-04 22:43:10 -0400

    Update group.java

--- group.java
+++ group.java
@@ -1,13 +1,27 @@
 public class group {
 	
-	public void getGroupMembers() {
+	public static void getGroupMembers() {
 		System.out.println("Anthony Mikulcic");
 		System.out.println("Brandon Wendell");
-        	System.out.println("Drew Robinson");
+        System.out.println("Drew Robinson");
+        System.out.println("Dylan Monreal");
 	}
 	
-
-    public int power(int num1, int num2) {
+	public static void main(String args[]) {
+		getGroupMembers();
+		System.out.println(multiply(3,2));
+		System.out.println(power(3,2));
+	}
+	
+	public static int multiply(int num1, int num2) {
+		int sum = 0;
+		for(int i = 1; i <= num1; i++) {
+			sum = sum + num2;
+		}
+		return sum;
+	}
+	
+    public static int power(int num1, int num2) {
         if (num2 < 0) {
             throw new IllegalArgumentException("Negative exponent not supported");
         }
