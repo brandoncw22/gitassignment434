0c38c9a899429a6d9e7b9cf4cb32489a76f98c89
brandoncw22 <162731539+brandoncw22@users.noreply.github.com>
2024-04-04 22:03:40 -0400

    Update group.java

--- group.java
+++ group.java
@@ -1,2 +1,17 @@
 public class group {
+    public int power(int num1, int num2) {
+        if (num2 < 0) {
+            throw new IllegalArgumentException("Negative exponent not supported");
+        }
+        if (num1 == 0 && num2 == 0) {
+            throw new IllegalArgumentException("0^0 is undefined");
+        }
+
+        int result = 1;
+        for (int i = 0; i < num2; i++) {
+            result = multiply(result, num1);
+        }
+
+        return result;
+    }
 }
